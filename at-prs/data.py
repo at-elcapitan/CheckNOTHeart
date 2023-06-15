1681622295e4cb738506773655fcd310772f2b7f
at-elcapitan <gameforceman1808@gmail.com>
2023-06-15 22:14:36 +0300

    at-prs_150620232214

--- at-prs/data.py
+++ at-prs/data.py
@@ -10,16 +10,41 @@ import functools
 from flask import (
     Blueprint, g, flash, redirect, render_template, request, session, url_for
 )
+from datetime import datetime
 from .database import get_database
 from .authentication import login_required
 
 bp = Blueprint("index", __name__)
 
 
-@bp.route('/')
+@bp.route('/', methods=('GET', 'POST'))
 @login_required
 def data_index():
-    return render_template('index.html', userid = session['user_id'])
+    db = get_database()
+    req = db.execute('SELECT first_name, last_name, last_press, last_rate, img_url, admin_override FROM users WHERE id = ?', (session['user_id'],)).fetchone()
+
+    if request.method == "POST":
+        highpr = request.form.get("highpr", None)
+        lowpr = request.form.get("lowpr", None)
+        hrate = request.form.get("heartrate", None)
+        health = bool(request.form.get("health", False))
+        error = None
+        
+        try: 
+            highpr = int(highpr)
+            lowpr = int(lowpr)
+            hrate = int(hrate)
+        except: error = "Preasure or heart rate must be a number"
+
+        if error is None:
+            db.execute('INSERT INTO data_press (date, press_high, press_low, heart_rate, user_state, user_id) VALUES (?, ?, ?, ?, ?, ?)',
+                       (datetime.now(), highpr, lowpr, hrate, health, session['user_id']))
+            db.execute('UPDATE users SET last_press = ?, last_rate = ? WHERE id = ?', (f'{highpr}/{lowpr}', hrate, session['user_id']))
+            db.commit()
+        
+        return redirect(url_for('index.data_index'))
+
+    return render_template('index.html', userid = session['user_id'], uinfo = req, isadmin = bool(req['admin_override']))
 
 
 @bp.route('/about')
--- at-prs/database.sql
+++ at-prs/database.sql
@@ -1,11 +1,3 @@
-// at-prs (c) by Vladislav 'ElCapitan' Nazarov
-// 
-// at-prs is licensed under a
-// Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
-// 
-// You should have received a copy of the license along with this
-// work.  If not, see <http://creativecommons.org/licenses/by-nc-sa/3.0/>.
-
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS data_press;
 
@@ -14,8 +6,10 @@ CREATE TABLE users (
     username TEXT UNIQUE NOT NULL,
     password INTEGER NOT NULL,
     first_name TEXT NOT NULL,
-    middle_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
+    last_press TEXT DEFAULT "NONE",
+    last_rate TEXT DEFAULT "NONE",
+    img_url TEXT,
     admin_override BOOL NOT NULL
 );
 
@@ -24,9 +18,10 @@ CREATE TABLE data_press (
     date SMALLDATETIME NOT NULL,
     press_high INTEGER NOT NULL,
     press_low INTEGER NOT NULL,
+    heart_rate INTEGER NOT NULL,
     user_state BOOL NOT NULL,
     user_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
-INSERT INTO users (username, password, first_name, middle_name, last_name, admin_override) VALUES ('root', 'pbkdf2:sha256:600000$x2BbGmc8Uj0PVPnY$275897aa0272aa2d247a8c856c8153d94264b5b52b6ef55fe7cfb899f50bac75', ' ', 'SYSTEM', 'USER', 1);
+INSERT INTO users (username, password, first_name, last_name, admin_override) VALUES ('root', 'pbkdf2:sha256:600000$x2BbGmc8Uj0PVPnY$275897aa0272aa2d247a8c856c8153d94264b5b52b6ef55fe7cfb899f50bac75', 'SYSTEM', 'USER', 1);
