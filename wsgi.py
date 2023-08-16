d6257b9bf7c17470ba59329130dff061ca76e66d
at-elcapitan <gameforceman1808@gmail.com>
2023-08-16 16:08:51 +0300

    Changes folder name; Created wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,14 @@
+# xnx-cnh (c) by Vladislav 'ElCapitan' Nazarov
+# 
+# xnx-cnh is licensed under a
+# Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+# 
+# You should have received a copy of the license along with this
+# work.  If not, see <http://creativecommons.org/licenses/by-nc-sa/3.0/>.
+
+from xnxcnh import create_app
+app = create_app()
+
+if __name__ == "__main__":
+    app.run()
+
